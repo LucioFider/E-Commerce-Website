edc55d36ca1ec7b3b6db68c56d284fd673d1f62e
LucioFider <fidelislucio404@gmail.com>
2023-02-05 10:17:14 +1000

    3rd-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  About,
+  Cart,
+  Auth,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages";
+
+import styled from "styled-components";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<Home />} />)
+);
 
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from "./AboutPage";
+import Home from "./HomePage";
+import Cart from "./CartPage";
+import Auth from "./AuthWrapper";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export {
+  Home,
+  About,
+  Cart,
+  Auth,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+};
